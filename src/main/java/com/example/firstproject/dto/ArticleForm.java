f41d314adc5cd8ded248dc1c8b64c1582ccd94af
yoonjeong <yonjeong5180@gmail.com>
2024-11-06 23:28:30 +0900

    24.11.06

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,28 +1,15 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor // 롬복 어노테이션 추가 (생성자 코드 대체)
+@ToString // 롬복 어노테이션 추가2 (toString 코드 대체)
 public class ArticleForm
 {
     private String title; // 제목을 받을 필드
     private String content; // 내용을 받을 필드
-
-    // 전송받은 제목과 내용을 필드에 저장하는 생성자 추가
-    public ArticleForm(String title, String content)
-    {
-        this.title = title;
-        this.content = content;
-    }
-
-    // 데이터를 잘 받았느지 확인할 toString() 메서드 추가
-    @Override
-    public String toString()
-    {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity()
     {
