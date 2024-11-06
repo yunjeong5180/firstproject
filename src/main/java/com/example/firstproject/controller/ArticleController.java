c71f35aedcba4ce9c282261f7110dde3372f87ea
yoonjeong <yonjeong5180@gmail.com>
2024-11-06 13:20:05 +0900

    24.11.06

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article; // Article 클래스 임포트 확인
 import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ArticleController
 {
+    // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
+    @Autowired
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
@@ -24,8 +27,12 @@ public class ArticleController
         System.out.println(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
+        // DTO가 엔티티로 잘 변환되는지 확인 출력
+        System.out.println(article.toString());
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
+        // article이 DB에 잘 저장되는지 확인 출력
+        System.out.println(saved.toString());
         return "";
     }
 }
